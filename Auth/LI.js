f815657b051459550490495a2c3d249d7898f6a0
LuongHoangThongUwU <lht27076@gmail.com>
2026-07-25 01:07:30 +0700

    fix header

--- Auth/LI.js
+++ Auth/LI.js
@@ -143,3 +143,17 @@ function logout(){
 
     window.location.href = "LI.html";
 }
+
+
+// Them Header va Footer
+fetch("../Components/Header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("header").innerHTML = data;
+    });
+
+fetch("../Components/Footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data;
+    });
